c9030009b8a5e5a002a4b0ef0f61e82d339050c7
Jorg8383 <joerg.striebel@icloud.com>
2023-08-14 12:19:40 +0200

    feat(bmp180): create source file for the Bosch BMP180 sensor API

---
+++ bmp180.c
@@ -0,0 +1,21 @@
+/**
+ * 
+ * File:                bmp180.c
+ * Author:              J. Striebel
+ * Project:             Weather Station
+ * Device:              PIC18F47Q10 @ 16 MHz internal OSC, 5V
+ * Platform:            Curiosity HPC board (DM164136)
+ * Sensor:              Bosch BMP180 Digital pressure sensor 
+ * Compiler:            XC8 (v2.41)
+ * IDE:                 MPLAB X (v6.10), MCC (5.3.7)
+ * Program version:     1.0
+ 
+ * Program Description:
+ * This module represents an Application Programming Interface (API) for the 
+ * Bosch BMP180 digital pressure sensor.
+ * 
+ * 
+ * Hardware Description:
+ * 
+*/
+
