ebd9431b14968ac742670187a15690f742beb480
Jörg Striebel <92162900+Jorg8383@users.noreply.github.com>
2023-08-17 16:13:52 +0200

    docs(bmp180): add documentation to the BMP180 header file

--- bmp180.h
+++ bmp180.h
@@ -10,9 +10,15 @@
  
  * Program Description:
  * --------------------
+ * This header file represents an Application Programming Interface (API) for
+ * the Bosch BMP180 barometric pressure sensor, allowing to communicate with
+ * a MCU via I2C. To ensure application code portability across various 
+ * Microchip MCU's, this sensor API utilises the Foundation Service framework
+ * which provides fully hardware-abstracted interfaces. 
  * 
  * Hardware Description:
  * --------------------
+ * Bosch barometric pressure sensor BMP180 with an I2C interface
  *    
  */
 
@@ -119,11 +125,101 @@ typedef struct bmp180_param_t
 } BMP180_PARAM; 
 
 
-/* Function prototype declarations */
+/******************************************************************************* 
+ * Function to initialise the communication with the BMP180 sensor
+ ******************************************************************************/
+/*
+ * @brief This function is used to initialise the communication with the BMP180
+ * sensor by reading calibration coefficients from its internal EEPROM
+ * 
+ * @param bmp180 -> pointer to BMP180 data structure 
+ * 
+ * @return the result of invoking the initialisation routine
+ * @retval 0 -> success
+ * @retval 1 -> failed; null pointer to BMP180 data structure was provided
+ * @retval 2 -> failed; couldn't establish communication with the sensor
+ * 
+ * @note
+ * The following calibration parameters are read from the sensor's EEPROM:
+ *  Parameter  |  MSB    |  LSB    
+ * ------------|---------|---------
+ *      AC1    |  0xAA   | 0xAB
+ *      AC2    |  0xAC   | 0xAD
+ *      AC3    |  0xAE   | 0xAF
+ *      AC4    |  0xB0   | 0xB1
+ *      AC5    |  0xB2   | 0xB3
+ *      AC6    |  0xB4   | 0xB5
+ *      B1     |  0xB6   | 0xB7
+ *      B2     |  0xB8   | 0xB9
+ *      MB     |  0xBA   | 0xBB
+ *      MC     |  0xBC   | 0xBD
+ *      MD     | 0xBE    | 0xBF
+ * 
+ */
 uint8_t BMP180_Init(BMP180_PARAM *bmp180);
+
+
+/******************************************************************************* 
+ * Function to read uncompensated temperature
+ ******************************************************************************/
+/*
+ * @brief This function is used to read the uncompensated temperature from the
+ * registers 0xF6 and 0xF7
+ * 
+ * @param None
+ * 
+ * @return The uncompensated temperature
+ * 
+*/
 uint16_t BMP180_ReadRawTemperature(void);
+
+
+/******************************************************************************* 
+ * Function to read uncompensated pressure
+ ******************************************************************************/
+/*
+ * @brief This function is used to read the uncompensated temperature from the
+ * registers 0xF6 (MSB), 0xF7 (LSB), and 0xF8 (XLSB) 
+ * 
+ * @param None
+ * 
+ * @return The uncompensated pressure
+ * 
+*/
+
 uint32_t BMP180_ReadRawPressure(void);
+
+
+/******************************************************************************* 
+ * Function to calculate the true temperature
+ ******************************************************************************/
+/*
+ * @brief This function calculates the true temperature in Celsius degree based 
+ * on the provided raw temperature. Therefore, the raw temperature must be read
+ * before invoking. 
+ * 
+ * @param The result from invoking BMP180_ReadRawPressure(void)
+ * 
+ * @return The true temperature as a 16-bit signed integer in 0.1 Celsius degree
+ * steps
+ * 
+*/
 int16_t BMP180_CalcTemperature(uint16_t rawTemperature);
+
+
+/******************************************************************************* 
+ * Function to calculate the true pressure
+ ******************************************************************************/
+/*
+ * @brief This function calculates the true pressure based on the provided
+ * raw pressure and raw temperature. Therefore, these parameters must be read 
+ * before invoking this function. 
+ * 
+ * @param The result from invoking BMP180_ReadRawPressure(void)
+ * 
+ * @return The true pressure in Pa as a 32-bit signed integer
+ * 
+*/
 int32_t BMP180_CalcPressure(uint32_t rawPressure, uint32_t rawTemperature);
 
 
