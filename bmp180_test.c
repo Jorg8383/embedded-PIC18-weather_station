b2ee124e64652014e23d69519b95f9feb03bb49e
Jörg Striebel <92162900+Jorg8383@users.noreply.github.com>
2023-08-18 11:34:09 +0200

    test(bmp180): enhance testing routine

--- bmp180_test.c
+++ bmp180_test.c
@@ -21,11 +21,11 @@
 #include "bmp180.h"
 
 BMP180_PARAM bmp180;
+_Bool initialiseSensor = true;
+uint8_t initResult = 0xFF;
 
 void BMP180_TestRoutine(void){
 
-    _Bool initialiseSensor = true;
-    uint8_t initResult;
     uint16_t rawTemperature;
     uint32_t rawPressure;
     int16_t temperature;
@@ -37,6 +37,26 @@ void BMP180_TestRoutine(void){
         printf("BMP180 initialisation result: %d\n", initResult);
     }
     
+    __delay_ms(100);
+    
+    if (initResult == 0){
+        
+        rawTemperature = BMP180_ReadRawTemperature();
+        printf("BMP180 - raw temperature: %d\n", rawTemperature);
+
+        rawPressure = BMP180_ReadRawPressure();
+        printf("BMP180 - raw pressure: %d\n", rawPressure);
+        
+        temperature = BMP180_CalcTemperature(rawTemperature);
+        printf("BMP180 - temperature: %d\n", temperature);        
+        
+        pressure = BMP180_CalcPressure(rawPressure, rawTemperature);
+        printf("BMP180 - pressure: %d\n", pressure);
+        
+        printf("----------------------------------\n");
+    }
+
+    __delay_ms(2000);
     
     
 }
